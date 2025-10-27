c8c3b0e11e34986de1d7a20ffdc5f5662d4b5709
Nicorrax12 <thetodito123@gmail.com>
2025-10-26 22:03:57 -0500

    Delete matematicas directory

--- matematicas/conexion.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1606@localhost/matematicas'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
